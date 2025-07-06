c249af4c92e116265af7888e318c8ef951de19a8
Gabrielbuddy <j.gabrielquintanact@gmail.com>
2025-07-06 12:49:26 -0300

    Update whatsapp.js

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -1,20 +1,17 @@
 export default function handler(req, res) {
-  const VERIFY_TOKEN = "fernandobot";
+  if (req.method === 'GET') {
+    const VERIFY_TOKEN = 'fernandobot';
 
-  if (req.method === "GET") {
-    const mode = req.query["hub.mode"];
-    const token = req.query["hub.verify_token"];
-    const challenge = req.query["hub.challenge"];
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
 
-    if (mode && token) {
-      if (mode === "subscribe" && token === VERIFY_TOKEN) {
-        console.log("WEBHOOK_VERIFIED");
-        res.status(200).send(challenge);
-      } else {
-        res.sendStatus(403);
-      }
+    if (mode && token === VERIFY_TOKEN) {
+      return res.status(200).send(challenge);
+    } else {
+      return res.status(403).send('Forbidden');
     }
   } else {
-    res.sendStatus(404);
+    return res.status(405).send('Method Not Allowed');
   }
 }
