8c363dd69d4fe3cb711f72cb17ab5e0865de4583
Gabrielbuddy <j.gabrielquintanact@gmail.com>
2025-07-06 12:46:44 -0300

    Update whatsapp.js

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -6,12 +6,15 @@ export default function handler(req, res) {
     const token = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
 
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      return res.status(200).send(challenge);
-    } else {
-      return res.status(403).send("Forbidden");
+    if (mode && token) {
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        res.sendStatus(403);
+      }
     }
+  } else {
+    res.sendStatus(404);
   }
-
-  return res.status(200).send("OK");
 }
