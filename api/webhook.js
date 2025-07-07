37af950636274f991134e36366dad5e8440c4002
Gabrielbuddy <j.gabrielquintanact@gmail.com>
2025-07-07 09:38:31 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,6 +2,6 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     res.status(200).json({ message: 'Webhook recebido com sucesso!' });
   } else {
-    res.status(405).json({ message: 'Método não permitido' });
+    res.status(200).json({ message: 'Método não permitido' });
   }
 }
