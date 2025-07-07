0c6a73051bb281f1733a2f794ac586e0032f8a03
Gabrielbuddy <j.gabrielquintanact@gmail.com>
2025-07-07 09:31:59 -0300

    Add files via upload

---
+++ api/webhook.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+  } else {
+    res.status(405).json({ message: 'Método não permitido' });
+  }
+}
