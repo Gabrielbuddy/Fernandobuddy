4f91111682e712c1ed1f020b180d754dc87997d6
Gabrielbuddy <j.gabrielquintanact@gmail.com>
2025-07-07 09:50:51 -0300

    Delete api/webhook.js

--- api/webhook.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json({ message: 'Webhook recebido com sucesso!' });
-  } else {
-    res.status(200).json({ message: 'Método não permitido' });
-  }
-}
